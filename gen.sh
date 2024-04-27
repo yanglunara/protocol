ccf16886a7092c5956e7d709633e091251f8954f
yanghao89 <1491286207@qq.com>
2024-04-27 16:26:28 +0800

    protocol

---
+++ gen.sh
@@ -0,0 +1,17 @@
+PROTO_NAMES=(
+    "auth"
+)
+for name in "${PROTO_NAMES[@]}"; do
+  protoc --go_out=./${name} --go_opt=module=github.com/yanglunara/protocol/${name} --go-grpc_out=./${name} --go-grpc_opt=module=github.com/yanglunara/protocol/${name} ${name}/${name}.proto
+  
+  if [ $? -ne 0 ]; then
+      echo "error processing ${name}.proto"
+      exit $?
+  fi
+done
+
+if [ "$(uname -s)" == "Darwin" ]; then
+    find . -type f -name '*.pb.go' -exec sed -i '' 's/,omitempty"`/\"\`/g' {} +
+else
+    find . -type f -name '*.pb.go' -exec sed -i 's/,omitempty"`/\"\`/g' {} +
+fi
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/yanglunara/protocol
+
+go 1.22.2
+
+require (
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.3.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+)
